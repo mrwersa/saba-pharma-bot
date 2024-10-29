56307fc0bf95407b805ae39d483c28d4c1154efb
Saeed Aghaee <aghaeester@gmail.com>
2024-10-29 04:13:39 +0000

    Update pharmacy_data.py

--- pharmacy_data.py
+++ pharmacy_data.py
@@ -27,8 +27,14 @@ def get_custom_chrome_options():
     chrome_options = webdriver.ChromeOptions()
     
     # Run in headless mode
-    chrome_options.add_argument("--headless")
-    
+    chrome_options.add_argument("--headless")  # Enable headless mode
+    chrome_options.add_argument("--no-sandbox")  # Disable sandboxing for headless
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
+    # Set logging level
+    chrome_options.add_argument("--log-level=ALL")
+    chrome_options.add_argument("--verbose")
+
     # Randomize User-Agent
     user_agent = random.choice(USER_AGENTS)
     chrome_options.add_argument(f"user-agent={user_agent}")
